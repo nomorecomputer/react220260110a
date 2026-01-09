682204a20b0ee4c1818677969af8100da6d1ab7a
WeiLin <nomorecomputer@gmail.com>
2026-01-10 01:28:36 +0800

    fix production base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import process from "process";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: { open: true },
+  base: process.env.NODE_ENV === "production" ? "/react220260110a/" : "/",
   plugins: [react()],
 });
